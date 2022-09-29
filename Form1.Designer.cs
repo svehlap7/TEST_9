363d18c1d7c57b9b558fab68c49863c136186436
brpgrrtygng <74527999+svehlap7@users.noreply.github.com>
2022-09-29 10:53:07 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace TEST_BINAR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(188, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "VYPIS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(188, 67);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 199);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(314, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 49);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "OPRAV";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(314, 67);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 199);
+            this.listBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(87, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "RANDOM cisla:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(440, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = ":OPRAVENA cisla";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Crimson;
+            this.ClientSize = new System.Drawing.Size(636, 303);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TEST1";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TEST_BINAR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int posledni;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileStream stream = new FileStream(@"..\..\cisla.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryWriter binarpis = new BinaryWriter(stream);
+            BinaryReader binarcti = new BinaryReader(stream);
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int cisla = random.Next(-10, 11);
+                binarpis.Write(cisla);
+                posledni = cisla;
+            }
+
+            binarpis.Seek(0, SeekOrigin.Begin);
+            listBox1.Items.Clear();
+
+            while (binarcti.BaseStream.Position < binarcti.BaseStream.Length)
+            {
+                int x = binarcti.ReadInt32();
+                listBox1.Items.Add(x);
+            }
+            stream.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+
+            FileStream stream = new FileStream(@"..\..\cisla.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryReader binarcti = new BinaryReader(stream);
+            BinaryWriter binarpis = new BinaryWriter(stream);
+
+             binarpis.Seek(0, SeekOrigin.Begin);
+
+            while (binarcti.BaseStream.Position < binarcti.BaseStream.Length)
+            {
+                int cisla = binarcti.ReadInt32();
+                if (posledni % 2 != 0)
+                {
+                    if (cisla % 2 == 0)
+                    {
+                        cisla = cisla + 1;
+                    }
+                }
+                else
+                {
+                    if (cisla % 2 != 0)
+                    {
+                        cisla = cisla * 2;
+                    }
+                }
+                binarpis.BaseStream.Position -= 4;
+                binarpis.Write(cisla);
+            }
+            stream.Close();
+
+            FileStream stream2 = new FileStream(@"..\..\cisla.dat", FileMode.Open, FileAccess.Read);
+            BinaryReader binarcti2= new BinaryReader(stream2);
+
+            while (binarcti2.BaseStream.Position < binarcti2.BaseStream.Length)
+            {
+                int cisla = binarcti2.ReadInt32();
+                listBox2.Items.Add(cisla);
+            }
+            stream2.Close();
+        }
+    }
+}
